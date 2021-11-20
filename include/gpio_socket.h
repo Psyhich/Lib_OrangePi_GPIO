1803576d2732779dffcfffe4c35bfb4dae85a3eb
Kai Kosades <red4truck@tutanota.com>
2021-11-20 22:04:45 +0200

    Implemented read and write functions

--- include/gpio_socket.h
+++ include/gpio_socket.h
@@ -6,9 +6,11 @@
 #include <optional>
 #include <string>
 #include <vector>
+#include <thread>
 
 namespace GPIOSockets {
 	using byte = unsigned char;
+	using callbackFunc = bool(*)(char);
 
 	constexpr const char* SYS_GPIO_PATH = GPIO_PATH;
 	constexpr const char* SYS_GPIO_EXPORT_PATH = GPIO_PATH "export";
@@ -17,17 +19,32 @@ namespace GPIOSockets {
 	class GPIOSocket {
 	public:
 		GPIOSocket(std::string path);
+		~GPIOSocket();
 		
-		std::optional<char> read();
-		bool write(const std::vector<char> &arrayToWrite);
+		std::optional<char> read() noexcept;
+		void continiousRead(callbackFunc callback);
+		bool write(const std::vector<char> &arrayToWrite) noexcept;
+		bool write(char charToWrite) noexcept;
 
 		inline std::string getPath() const noexcept { return m_path; }
+		inline bool isReading() const noexcept { return m_runningThread != nullptr; }
 
-		static bool openGPIO(int portToOpen);
-		static bool closeGPIO(int portToOpen);
+		inline void joinReading(){
+			if(isReading() && m_runningThread->joinable()){
+				m_runningThread->join();
+			}
+		}
+		void stopReading();
+
+		static bool openGPIO(int portToOpen) noexcept;
+		static bool closeGPIO(int portToOpen) noexcept;
 	protected:
 	private:
+		static void readFunction(callbackFunc callback, const bool& shouldStop, const std::string &path);
 		std::string m_path;
+
+		std::thread *m_runningThread{nullptr};
+		bool m_shouldStop{false};
 	};
 }
 
--- src/gpio_socket.cpp
+++ src/gpio_socket.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <iostream>
 
+#include <poll.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #include "gpio_socket.h"
 
 using namespace GPIOSockets;
@@ -9,23 +13,85 @@ using namespace GPIOSockets;
 GPIOSocket::GPIOSocket(std::string path) {
 	m_path = SYS_GPIO_PATH + path;
 }
+GPIOSocket::~GPIOSocket() {
+	if(isReading()){
+		m_shouldStop = true;
+		if(m_runningThread->joinable()){
+			m_runningThread->join();
+		}
+		delete m_runningThread;
+	}
+}
 
-std::optional<char> GPIOSocket::read() {
+std::optional<char> GPIOSocket::read() noexcept {
 	std::optional<byte> readData = std::nullopt;
+
+	if(isReading()){
+		printf("Tried to run second read on single socket\n");
+		return readData;
+	}
+
 	try { 
 		std::ifstream socketToRead{m_path + "/value"};
 		char byteToRead;
-		if(!socketToRead.get(byteToRead).eof()){
-			readData = byteToRead;
-		}
+		socketToRead.get(byteToRead);
+		readData = byteToRead;
 	} catch(std::exception&) {
-
+		printf("Error while reading single byte\n");
 	}
 
 	return readData;
 }
 
-bool GPIOSocket::write(const std::vector<char> &arraToWrite) {
+void GPIOSocket::readFunction(callbackFunc callback, const bool& shouldStop, const std::string &path) {
+	FILE* socketToRead = fopen(path.data(), "r");
+	if(socketToRead == nullptr) {
+		printf("Failed to open file\nPath: %s\n", path.data());
+		return;
+	}
+	pollfd gpioPollFD;
+	gpioPollFD.fd = fileno(socketToRead);
+	gpioPollFD.events = POLLIN;
+
+	char readByte;
+	bool callbackStop = false;
+	int pollValue;
+
+	do {
+		pollValue = poll(&gpioPollFD, 1, -1);
+		if(pollValue < 0){ continue; }
+			
+		if( (gpioPollFD.revents & POLLIN) == POLLIN){
+			lseek(gpioPollFD.fd, 0, SEEK_SET);
+			if(std::fread(&readByte, sizeof(readByte), 1, socketToRead) &&
+				readByte != '\0' && readByte != '\n'){
+				callbackStop = callback(readByte);
+			}
+
+		}
+	} while(!shouldStop && !callbackStop);
+}
+
+void GPIOSocket::continiousRead(callbackFunc callback){
+	if(isReading()){
+		printf("Tried to run second continious read on single socket\n");
+		return;
+	}
+	m_shouldStop = false;
+	m_runningThread = new std::thread(readFunction, callback, this->m_shouldStop, getPath() + "/value");
+}
+
+void GPIOSocket::stopReading() {
+	if(isReading()){
+		m_shouldStop = true;
+		m_runningThread->join();
+		delete m_runningThread;
+	} else {
+		printf("Tried to stop reading that didn't start\n");
+	}
+}
+
+bool GPIOSocket::write(const std::vector<char> &arraToWrite) noexcept{
 	try {
 		std::ofstream socketToWrite{m_path + "/value"};
 
@@ -39,7 +105,19 @@ bool GPIOSocket::write(const std::vector<char> &arraToWrite) {
 	return true;
 }
 
-bool GPIOSocket::openGPIO(int portToOpen){
+bool GPIOSocket::write(char charToWrite) noexcept{
+	try {
+		std::ofstream socketToWrite{m_path + "/value"};
+
+		socketToWrite.put(charToWrite);
+		socketToWrite.flush();
+	} catch (std::exception&) {
+		return false;
+	}
+	return true;
+}
+
+bool GPIOSocket::openGPIO(int portToOpen) noexcept {
 	try {
 		std::ofstream exportSocket{SYS_GPIO_EXPORT_PATH};
 		std::string socketNumber = std::to_string(portToOpen); // To work with file sockets we need to write string value
@@ -50,7 +128,7 @@ bool GPIOSocket::openGPIO(int portToOpen){
 	return true;
 }
 
-bool GPIOSocket::closeGPIO(int portToClose){
+bool GPIOSocket::closeGPIO(int portToClose) noexcept {
 	try {
 		std::ofstream unexportSocket{SYS_GPIO_UNEXPORT_PATH};
 		std::string socketNumber = std::to_string(portToClose); // To work with file sockets we need to write string value
@@ -59,7 +137,5 @@ bool GPIOSocket::closeGPIO(int portToClose){
 		return false;
 	}
 	return true;
-
 }
-
 
